cd21d4edc0e10d92fe890b2541e254496a7c47cf
Shodiqul Muzaki <user.creativefull@gmail.com>
2016-05-23 15:08:33 +0700

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # demo-api
 Demo tutorial membuat api
+
+# Cara Menggunakan
+```
+git clone https://github.com/creativefull/demo-api
+```
+kemudian masuk di folder demo-api
+
+```
+npm install
+```
+
+# Restore Database
+
+```
+mongorestore -d cermai database/cermai
+```
+
+# Route
+
+1. /api/auth
+2. /api/item
+3. /api/userinfo
+
+# Support
+http://tangituru.com
+
+# Developer
+Shodiqul Muzaki
---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var mongodb = require('mongodb').MongoClient;
+var bodyParser = require('body-parser');
+var app = express();
+
+mongodb.connect('mongodb://localhost:27017/cermai', function(err, db) {
+	if (err) throw err;
+
+	var ApiHandler = require('./routes/api');
+	var API = new ApiHandler(db);
+
+	app.use(bodyParser.json());
+	app.use(bodyParser.urlencoded({ extended: false }));
+
+	app.get("/", API.index);
+	app.post("/api/auth", API.auth);
+
+	app.get("/api/item", API.isAuth, API.item);
+
+	app.listen(2016, function() {
+		console.log("Application Listen on port ", 2016)
+	})
+})
